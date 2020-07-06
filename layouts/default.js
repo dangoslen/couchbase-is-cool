387e78308d235a7acd81d7b8f13a5257cfe9fbe7
Dan Goslen <dgoslen@bandwidth.com>
2020-07-06 11:08:31 -0400

    Overring default font-sizes

--- layouts/default.js
+++ layouts/default.js
@@ -1,14 +1,15 @@
 // example Layout.js
 import React from 'react'
-import '../themes/css/prism.css'
+import '../themes/css/screen.css'
 
 const Preso = ({ children }) => {
   return (
     <div
-        style={{
-          width: '75vw',
-          height: '80vh',
-        }}
+      className='slide'
+      style={{
+        width: '75vw',
+        height: '80vh',
+      }}
     >
       <div>{children}</div>
       <div
